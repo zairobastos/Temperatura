49a41c630067858d9c074808b28cff50583d8225
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-01-01 15:51:23 -0300

    Ignorando Node_modules

---
+++ assets/js/api.js
@@ -0,0 +1,49 @@
+const seacher = document.querySelector('#cidade');
+const submit = document.querySelector('.pesquisa');
+
+/* const city = submit.addEventListener('click', function (){
+        
+    
+})
+ */
+
+const URL = `http://api.weatherapi.com/v1/forecast.json?key=ed600e62357c49db8b5205211213012&q=sobral&days=3&aqi=no&alerts=no&lang=pt`;
+
+    const img = document.querySelector('.img-clima');
+    const temp = document.querySelector('h1');
+    const city = document.querySelector('.city h2');
+    const day = document.querySelector('.city .day');
+    const getWeather = async () => {
+        try {
+
+            const data = await fetch(URL);
+            const json = await data.json();
+            console.log(json);
+
+            moment.locale('pt-br');
+            const datas = moment().format('dddd, H');
+
+            const dados = {
+                image: json.current.condition.icon,
+                adress:{
+                    city: json.location.name,
+                    state: json.location.region,
+                },
+                temp:{
+                    celsius: json.current.temp_c,
+                    fahrenheit: json.current.temp_f,
+                },
+            }
+
+            img.src = dados.image;
+            temp.innerHTML = dados.temp.celsius;
+            city.innerHTML = `${dados.adress.city}, ${dados.adress.state}`
+            day.innerHTML = `${datas}:00`;
+
+            return dados;
+        } catch (error) {
+            console.log(error.name);
+        }
+    }
+
+    getWeather();
