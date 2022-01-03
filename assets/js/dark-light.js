4ed02714fd11d98aac9af67bfc1cbee3bf731aeb
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-01-03 15:45:50 -0300

    selecionar criado

--- assets/js/dark-light.js
+++ assets/js/dark-light.js
@@ -1,37 +1,3 @@
-const header = document.querySelector('.pesquisar');
-
-const tema = document.querySelector('#toggle');
-const body = document.querySelector('body');
-
-const celsius = document.querySelector('.estatistica .celsius');
-const fahrenheit = document.querySelector('.estatistica .fahrenheit');
-const temp = document.querySelector('h1');
-
-const chuva = document.querySelector('.chuva');
-const umidade = document.querySelector('.umidade');
-const vento = document.querySelector('.vento');
-
-const cidade = document.querySelector('.city h2');
-const day = document.querySelector('.city .day');
-const tempo = document.querySelector('.city .tempo');
-
-const temperatura = document.querySelector('.temperaturas');
-const chuvas = document.querySelector('.chuvas');
-const umidades = document.querySelector('.umidades');
-
-const diaAtivo = document.querySelector('.ativo-day');
-const dia1 = document.querySelector('.day1 .day');
-const dia2 = document.querySelector('.day2 .day');
-const dia3 = document.querySelector('.day3 .day');
-
-const max1 = document.querySelector('.day1 .max');
-const max2 = document.querySelector('.day2 .max');
-const max3 = document.querySelector('.day3 .max');
-
-const min1 = document.querySelector('.day1 .min');
-const min2 = document.querySelector('.day2 .min');
-const min3 = document.querySelector('.day3 .min');
-
 tema.addEventListener('click',()=>{
     const temaTexto = document.querySelector('.tema-nome')
     if(tema.checked){
@@ -41,9 +7,64 @@ tema.addEventListener('click',()=>{
         body.classList.toggle('body-light');
         header.classList.toggle('pesquisa-light');
 
-        celsius.classList.toggle('dark-texto');
         temp.classList.toggle('dark-texto');
-        fahrenheit.classList.toggle('dark-texto2');
+        if(celsius.classList.contains('ativo')){
+            celsius.classList.add('dark-texto');
+            celsius.classList.remove('dark-texto2');
+            celsius.classList.add('ativo');
+
+            fahrenheit.classList.add('dark-texto2');
+            fahrenheit.classList.remove('dark-texto');
+            fahrenheit.classList.remove('ativo')
+        }else if (!fahrenheit.classList.contains('ativo')){
+            celsius.classList.add('ativo');
+            celsius.classList.add('dark-texto');
+            celsius.classList.remove('dark-texto2');
+        }
+        if (fahrenheit.classList.contains('ativo')){
+            celsius.classList.remove('ativo');
+            celsius.classList.remove('dark-texto');
+            celsius.classList.add('dark-texto2');
+
+            fahrenheit.classList.add('dark-texto');
+            fahrenheit.classList.remove('dark-texto2');
+            fahrenheit.classList.add('ativo');
+        }else if(!celsius.classList.contains('ativo')){
+            fahrenheit.classList.add('dark-texto');
+            fahrenheit.classList.remove('dark-texto2');
+            fahrenheit.classList.add('ativo');
+        }
+
+        celsius.addEventListener('click',()=>{
+            if(celsius.classList.contains('ativo')){
+                celsius.classList.add('dark-texto');
+                celsius.classList.remove('dark-texto2');
+                celsius.classList.add('ativo');
+    
+                fahrenheit.classList.add('dark-texto2');
+                fahrenheit.classList.remove('dark-texto');
+                fahrenheit.classList.remove('ativo')
+            }else if (!fahrenheit.classList.contains('ativo')){
+                celsius.classList.add('ativo');
+                celsius.classList.add('dark-texto');
+                celsius.classList.remove('dark-texto2');
+            }
+        })
+        fahrenheit.addEventListener('click',()=>{
+            if (fahrenheit.classList.contains('ativo')){
+                celsius.classList.remove('ativo');
+                celsius.classList.remove('dark-texto');
+                celsius.classList.add('dark-texto2');
+    
+                fahrenheit.classList.add('dark-texto');
+                fahrenheit.classList.remove('dark-texto2');
+                fahrenheit.classList.add('ativo');
+            }else if(!celsius.classList.contains('ativo')){
+                fahrenheit.classList.add('dark-texto');
+                fahrenheit.classList.remove('dark-texto2');
+                fahrenheit.classList.add('ativo');
+            }
+        })
 
         chuva.classList.toggle('dark-texto2');
         umidade.classList.toggle('dark-texto2');
@@ -69,6 +90,7 @@ tema.addEventListener('click',()=>{
         min1.classList.toggle('dark-texto2');
         min2.classList.toggle('dark-texto2');
         min3.classList.toggle('dark-texto2');
+        
     }else{
         temaTexto.innerHTML = 'Tema: Dark';
         temaTexto.classList.toggle("dark-texto");
@@ -76,9 +98,33 @@ tema.addEventListener('click',()=>{
         body.classList.toggle('body-light');
         header.classList.toggle('pesquisa-light');
 
-        celsius.classList.toggle('dark-texto');
         temp.classList.toggle('dark-texto');
-        fahrenheit.classList.toggle('dark-texto2');
+        if(celsius.classList.contains('ativo')){
+            celsius.classList.remove('dark-texto');
+            celsius.classList.remove('dark-texto2');
+            celsius.classList.add('ativo');
+
+            fahrenheit.classList.remove('dark-texto2');
+            fahrenheit.classList.remove('dark-texto');
+            fahrenheit.classList.remove('ativo')
+        }else if(!fahrenheit.classList.contains('ativo')){
+            celsius.classList.add('ativo');
+            celsius.classList.remove('dark-texto');
+            celsius.classList.remove('dark-texto2');
+        }
+        if (fahrenheit.classList.contains('ativo')){
+            celsius.classList.remove('ativo');
+            celsius.classList.remove('dark-texto');
+            celsius.classList.remove('dark-texto2');
+
+            fahrenheit.classList.remove('dark-texto');
+            fahrenheit.classList.remove('dark-texto2');
+            fahrenheit.classList.add('ativo');
+        }else if(!celsius.classList.contains('ativo')){
+            fahrenheit.classList.add('ativo');
+            fahrenheit.classList.remove('dark-texto');
+            fahrenheit.classList.remove('dark-texto2');
+        }
 
         chuva.classList.toggle('dark-texto2');
         umidade.classList.toggle('dark-texto2');
---
+++ assets/js/selecionar.js
@@ -0,0 +1,33 @@
+const header = document.querySelector('.pesquisar');
+
+const tema = document.querySelector('#toggle');
+const body = document.querySelector('body');
+
+const celsius = document.querySelector('.estatistica .celsius');
+const fahrenheit = document.querySelector('.estatistica .fahrenheit');
+const temp = document.querySelector('h1');
+
+const chuva = document.querySelector('.chuva');
+const umidade = document.querySelector('.umidade');
+const vento = document.querySelector('.vento');
+
+const cidade = document.querySelector('.city h2');
+const day = document.querySelector('.city .day');
+const tempo = document.querySelector('.city .tempo');
+
+const temperatura = document.querySelector('.temperaturas');
+const chuvas = document.querySelector('.chuvas');
+const umidades = document.querySelector('.umidades');
+
+const diaAtivo = document.querySelector('.ativo-day');
+const dia1 = document.querySelector('.day1 .day');
+const dia2 = document.querySelector('.day2 .day');
+const dia3 = document.querySelector('.day3 .day');
+
+const max1 = document.querySelector('.day1 .max');
+const max2 = document.querySelector('.day2 .max');
+const max3 = document.querySelector('.day3 .max');
+
+const min1 = document.querySelector('.day1 .min');
+const min2 = document.querySelector('.day2 .min');
+const min3 = document.querySelector('.day3 .min');
