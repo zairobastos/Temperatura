15f88110de0b1fe7e8c37af8fb8cd17fce092bb5
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-01-02 23:20:47 -0300

    Finalizado a mudança de temas

--- assets/js/dark-light.js
+++ assets/js/dark-light.js
@@ -15,6 +15,23 @@ const cidade = document.querySelector('.city h2');
 const day = document.querySelector('.city .day');
 const tempo = document.querySelector('.city .tempo');
 
+const temperatura = document.querySelector('.temperaturas');
+const chuvas = document.querySelector('.chuvas');
+const umidades = document.querySelector('.umidades');
+
+const diaAtivo = document.querySelector('.ativo-day');
+const dia1 = document.querySelector('.day1 .day');
+const dia2 = document.querySelector('.day2 .day');
+const dia3 = document.querySelector('.day3 .day');
+
+const max1 = document.querySelector('.day1 .max');
+const max2 = document.querySelector('.day2 .max');
+const max3 = document.querySelector('.day3 .max');
+
+const min1 = document.querySelector('.day1 .min');
+const min2 = document.querySelector('.day2 .min');
+const min3 = document.querySelector('.day3 .min');
+
 tema.addEventListener('click',()=>{
     const temaTexto = document.querySelector('.tema-nome')
     if(tema.checked){
@@ -35,6 +52,23 @@ tema.addEventListener('click',()=>{
         cidade.classList.toggle('dark-texto');
         day.classList.toggle('dark-texto2');
         tempo.classList.toggle('dark-texto2');
+
+        temperatura.classList.toggle('dark-texto');
+        chuvas.classList.toggle('dark-texto');
+        umidades.classList.toggle('dark-texto');
+
+        diaAtivo.classList.toggle('background-light');
+        dia1.classList.toggle('dark-texto');
+        dia2.classList.toggle('dark-texto');
+        dia3.classList.toggle('dark-texto');
+
+        max1.classList.toggle('dark-texto');
+        max2.classList.toggle('dark-texto');
+        max3.classList.toggle('dark-texto');
+
+        min1.classList.toggle('dark-texto2');
+        min2.classList.toggle('dark-texto2');
+        min3.classList.toggle('dark-texto2');
     }else{
         temaTexto.innerHTML = 'Tema: Dark';
         temaTexto.classList.toggle("dark-texto");
@@ -53,5 +87,22 @@ tema.addEventListener('click',()=>{
         cidade.classList.toggle('dark-texto');
         day.classList.toggle('dark-texto2');
         tempo.classList.toggle('dark-texto2');
+
+        temperatura.classList.toggle('dark-texto');
+        chuvas.classList.toggle('dark-texto');
+        umidades.classList.toggle('dark-texto');
+
+        diaAtivo.classList.toggle('background-light');
+        dia1.classList.toggle('dark-texto');
+        dia2.classList.toggle('dark-texto');
+        dia3.classList.toggle('dark-texto');
+
+        max1.classList.toggle('dark-texto');
+        max2.classList.toggle('dark-texto');
+        max3.classList.toggle('dark-texto');
+
+        min1.classList.toggle('dark-texto2');
+        min2.classList.toggle('dark-texto2');
+        min3.classList.toggle('dark-texto2');
     }
 })
